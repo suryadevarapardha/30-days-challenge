613b33bb566473837b264b0998c08279c84af0e5
suryadevarapardha <suryadevarapardha@gmail.com>
2026-09-23 09:35:48 +0530

    Update prog1.c

--- program1/prog1.c
+++ program1/prog1.c
@@ -1,1 +1,13 @@
+include<stdio.h>
+int main(){
+float a,b,c,d,e,total,average,percentage;
+printf("enter the 5 subject marks:");
+scanf("%f %f %f %f %f", &a, &b, &c, &d, &e);
+total=a+b+c+d+e;
+average=(a+b+c+d+e)/5;
+percentage=(total/500)*100;
+printf("total:%f,average:%f,percentage:%f\n",total,average,percentage);
+return 0;
+}
+
 
